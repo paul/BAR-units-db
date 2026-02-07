d560bb46d2e30b35c1a3a9f7fb76d7f717c9ed16
Paul Sadauskas <p.sadauskas@plane.com>
2026-02-07 13:27:08 -0700

    Add luarocks config

---
+++ .luarocks/config-5.5.lua
@@ -0,0 +1,7 @@
+variables = {
+   LUA = "/Users/rando/.local/share/mise/installs/lua/5.5.0/bin/lua",
+   LUA_BINDIR = "/Users/rando/.local/share/mise/installs/lua/5.5.0/bin",
+   LUA_DIR = "/Users/rando/.local/share/mise/installs/lua/5.5.0",
+   LUA_INCDIR = "/Users/rando/.local/share/mise/installs/lua/5.5.0/include",
+   LUA_LIBDIR = "/Users/rando/.local/share/mise/installs/lua/5.5.0/lib"
+}
